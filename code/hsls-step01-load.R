f80e8d5b42b75c0feb374a670c2ea0d69d21bb4c
professornaite <professornaite@gmail.com>
2024-05-23 09:41:19 -0400

    name update

--- code/hsls-step01-load.R
+++ code/hsls-step01-load.R
@@ -1,9 +1,9 @@
 # load HSLS data for training
 
 library(readr)
-df <- read_csv("data/hsls_09_school_v1_0.csv")
-df
-head(df)
-tail(df)
-str(df)
-View(df)
+hsls_w1 <- read_csv("data/hsls_09_school_v1_0.csv")
+hsls_w1
+head(hsls_w1)
+tail(hsls_w1)
+str(hsls_w1)
+View(hsls_w1)
