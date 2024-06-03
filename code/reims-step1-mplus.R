59fab2410186003d68db975d3c57cf318f53e5d9
professornaite <professornaite@gmail.com>
2024-06-03 19:55:53 -0400

    update

--- code/reims-step1-mplus.R
+++ code/reims-step1-mplus.R
@@ -1,1 +1,13 @@
 # step 1: reims MPlus analysis
+
+library(devtools)
+install_github("michaelhallquist/MplusAutomation")
+
+# load packages
+library(MplusAutomation)
+library(tidyverse) #collection of R packages designed for data science
+library(here) #helps with filepaths
+library(janitor) #clean_names
+library(gt) # creates tables
+library(DiagrammeR) # create path diagrams
+
