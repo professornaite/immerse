c13faf7ce626d6410373860319c34ed1b30c105d
professornaite <professornaite@gmail.com>
2024-06-03 15:19:27 -0400

    subset data

--- code/reims-step0.R
+++ code/reims-step0.R
@@ -13,3 +13,19 @@ tail(reims)
 reims %>% 
   mutate_at(c(2:54), funs(recode(., `1`=0, `2`=0, `3`=1, `4`=1, .default = NaN))) -> reims.dummy
 reims.dummy
+df <- reims.dummy
+str(df)
+# label of factors
+# math identity: mathperson1 mathperson2 mathperson3 mathperson4 dislikemath pursuestem 
+# racial ethnic identity: learnrace knowrace proudrace unclearrace dontknowrace belongrace understandrace talkrace priderace practicerace strongrace feelrace
+# caring relations at school: adultcares adultnotices adultlistens
+# high expectations at school: adultgood adultbest adultsuccess
+# meaningful participation in school: interestingacts decidethings makediff
+# math self efficacy: mathnoadv mathbad mathpuzzle mathboring mathgrades mathimpt
+# racial belonging: closerace belongrace raceidentity raceeffects raceproud racehappy
+
+# subset the dichotomous data
+test.df <- df %>% 
+  select(c(1:54), age, sex, race) %>% 
+  relocate(groupflag, age, sex, race)
+test.df
