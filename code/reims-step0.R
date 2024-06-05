4cb6958e13cf4cb0303db1411dff1fc70180c565
professornaite <professornaite@gmail.com>
2024-06-05 17:31:24 -0400

    fix reims data

--- code/reims-step0.R
+++ code/reims-step0.R
@@ -9,15 +9,17 @@ View(reims)
 head(reims)
 tail(reims)
 
+table(reims$dislikemathclass)
+
 # create dichotomous indicators
 reims %>% 
   mutate_at(c(2:54), funs(recode(., `1`=0, `2`=0, `3`=1, `4`=1, .default = NaN))) -> reims.dummy
 reims.dummy
 
 # reverse code dislikemathclass
-reims.dummy$dislikemathclass <- ifelse(reims.dummy$dislikemathclass == 1, 0, 1)
+reims.dummy$dislikemathclass <- ifelse(reims.dummy$dislikemathclass == 1, 1, 0)
+table(reims.dummy$dislikemathclass)
 
-reims.dummy
 # label of factors
 # math identity: mathperson1 mathperson2 mathperson3 mathperson4 dislikemathclass pursuestem 
 # racial ethnic identity: learnrace knowrace proudrace unclearrace dontknowrace belongrace understandrace talkrace priderace practicerace strongrace feelrace
@@ -33,6 +35,8 @@ test.df <- reims.dummy %>%
   relocate(groupflag, age, sex, race) %>% 
   drop_na()
 test.df 
+
+table(test.df$dislikemathclass)
 
 # check for missing data
 test.df %>% 
